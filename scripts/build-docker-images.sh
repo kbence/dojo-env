6cbee8c42d7a4e3553d9a5cf2dff2bc04a8cdf5a
Bence Kiglics <bence.kiglics@gmail.com>
2014-08-03 08:58:27 +0200

    added option to build only the selected images

--- scripts/build-docker-images.sh
+++ scripts/build-docker-images.sh
@@ -7,8 +7,14 @@ sanitize_image_name()
     echo $1 | sed 's/#/sharp/g; s/+/plus/g; s/[^A-Za-z0-9_-]/_/g' | tr 'A-Z' 'a-z'
 }
 
-for image in $(ls $IMAGE_ROOT | grep -v build-essential); do
-  if [ -f "$IMAGE_ROOT/$image/Dockerfile" ]; then
+if [ "$#" -gt 0 ]; then
+  IMAGES="$@"
+else
+  IMAGES=$(ls $IMAGE_ROOT | grep -v build-essential)
+fi
+
+for image in $IMAGES; do
+  if [ -f "$IMAGE_ROOT/$image/Dockerfile" -a -f "$IMAGE_ROOT/$image/manifest.json" ]; then
     image_name=$(sanitize_image_name "$image")
     image_tag="$DOCKER_USERNAME/cyberdojo-$image_name"
     echo Building docker image "$image_tag"...
